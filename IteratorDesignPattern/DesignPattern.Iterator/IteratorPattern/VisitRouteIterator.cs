0dbd2c54437308b2bfad40c70f832820561072f9
edaagunes <edagunes00@hotmail.com>
2024-11-12 16:53:28 +0300

    Added Iterator Design Pattern

---
+++ IteratorDesignPattern/DesignPattern.Iterator/IteratorPattern/VisitRouteIterator.cs
@@ -0,0 +1,28 @@
+﻿namespace DesignPattern.Iterator.IteratorPattern
+{
+	public class VisitRouteIterator : IIterator<VisitRoute>
+	{
+		private VisitRouteMover _visitRouteMover;
+		public VisitRoute CurrentItem { get; set; }
+
+		private int currentIndex = 0;
+
+		public VisitRouteIterator(VisitRouteMover visitRouteMover)
+		{
+			_visitRouteMover = visitRouteMover;
+		}
+
+		public bool NextLocation()
+		{
+			if (currentIndex < _visitRouteMover.VisitRouteCount)
+			{
+				CurrentItem = _visitRouteMover.visitRoutes[currentIndex++];
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+}
---
+++ IteratorDesignPattern/DesignPattern.Iterator/IteratorPattern/VisitRouteMover.cs
@@ -0,0 +1,21 @@
+﻿namespace DesignPattern.Iterator.IteratorPattern
+{
+	public class VisitRouteMover : IMover<VisitRoute>
+	{
+		public List<VisitRoute> visitRoutes = new List<VisitRoute>();
+
+		// Yeni Route ekleme
+		public void AddVisitRoute(VisitRoute visitRoute)
+		{
+			visitRoutes.Add(visitRoute);
+		}
+
+		// Ziyaret edilen rota sayisini tutmak icin 
+		public int VisitRouteCount { get => visitRoutes.Count(); }
+
+		public IIterator<VisitRoute> CreateIterator()
+		{
+			return new VisitRouteIterator(this);
+		}
+	}
+}
