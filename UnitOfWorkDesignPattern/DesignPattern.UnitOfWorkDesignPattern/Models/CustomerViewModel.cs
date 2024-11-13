34bdfc4af09ce4037e40e46ba40ec9254457b599
edaagunes <edagunes00@hotmail.com>
2024-11-13 16:08:15 +0300

    Added Unit Of Work Design Pattern

---
+++ UnitOfWorkDesignPattern/DesignPattern.UnitOfWorkDesignPattern/Models/CustomerViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DesignPattern.UnitOfWorkDesignPattern.Models
+{
+    public class CustomerViewModel
+    {
+        public int SenderId { get; set; }
+        public int ReceiverId { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
