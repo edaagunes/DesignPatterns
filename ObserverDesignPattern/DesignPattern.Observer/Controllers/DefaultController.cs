aa8c8987e85a830451d4e94c753619ce581bfc49
edaagunes <edagunes00@hotmail.com>
2024-12-08 19:00:05 +0300

    Added Observer Design Pattern

---
+++ ObserverDesignPattern/DesignPattern.Observer/Controllers/DefaultController.cs
@@ -0,0 +1,48 @@
+﻿using DesignPattern.Observer.DAL;
+using DesignPattern.Observer.Models;
+using DesignPattern.Observer.ObserverPattern;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Observer.Controllers
+{
+	public class DefaultController : Controller
+	{
+		private readonly UserManager<AppUser> _userManager;
+		private readonly ObserverObject _observerObject;
+
+		public DefaultController(UserManager<AppUser> userManager, ObserverObject observerObject)
+		{
+			_userManager = userManager;
+			_observerObject = observerObject;
+		}
+
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View();
+		}
+		[HttpPost]
+		public async Task<IActionResult> Index(RegisterViewModel registerViewModel)
+		{
+			var appUser = new AppUser()
+			{
+				Name = registerViewModel.Name,
+				Email = registerViewModel.Email,
+				Surname = registerViewModel.Surname,
+				UserName = registerViewModel.Username
+			};
+
+			var result= await _userManager.CreateAsync(appUser,registerViewModel.Password);
+
+			if (result.Succeeded)
+			{
+				_observerObject.NotifyObserver(appUser);
+				return View();
+			}
+
+			return View();
+		}
+	}
+}
