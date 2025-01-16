2cd324b7b2a0bdd71494b6e4a4433bc6adfcded6
edaagunes <edagunes00@hotmail.com>
2025-01-16 18:01:03 +0300

    Added Decorator Design Pattern

---
+++ DecoratorDesignPattern/DesignPattern.Decorator/Controllers/DefaultController.cs
@@ -0,0 +1,52 @@
+﻿using DesignPattern.Decorator.DAL;
+using DesignPattern.Decorator.DecoratorPattern2;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DesignPattern.Decorator.Controllers
+{
+	public class DefaultController : Controller
+	{
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Index(Message message)
+		{
+			CreateNewMessage createNewMessage = new CreateNewMessage();
+			createNewMessage.SendMessage(message);
+			return View();
+		}
+
+		[HttpGet]
+		public IActionResult Index2()
+		{
+			return View();
+		}
+		[HttpPost]
+		public IActionResult Index2(Message message)
+		{
+			CreateNewMessage createNewMessage =new CreateNewMessage();
+			EncryptoBySubjectDecorator encryptoBySubjectDecorator = new EncryptoBySubjectDecorator(createNewMessage);
+			encryptoBySubjectDecorator.SendMessageByEncryptoSubject(message);
+			return View();
+		}
+
+
+		[HttpGet]
+		public IActionResult Index3()
+		{
+			return View();
+		}
+		[HttpPost]
+		public IActionResult Index3(Message message)
+		{
+			CreateNewMessage createNewMessage = new CreateNewMessage();
+			SubjectIdDecorator subjectIdDecorator = new SubjectIdDecorator(createNewMessage);
+			subjectIdDecorator.SendMessageByIdSubject(message);
+			return View();
+		}
+	}
+}
---
+++ DecoratorDesignPattern/DesignPattern.Decorator/DecoratorPattern2/SubjectIdDecorator.cs
@@ -0,0 +1,40 @@
+﻿using DesignPattern.Decorator.DAL;
+using System;
+
+namespace DesignPattern.Decorator.DecoratorPattern2
+{
+	public class SubjectIdDecorator : Decorator
+	{
+		private readonly ISendMessage _sendMessage;
+		Context context = new Context();
+
+		public SubjectIdDecorator(ISendMessage sendMessage) : base(sendMessage)
+		{
+			_sendMessage = sendMessage;
+		}
+
+		public void SendMessageByIdSubject(Message message)
+		{
+			if (message.MessageSubject=="1")
+			{
+				message.MessageSubject = "Toplantı";
+			}
+			if (message.MessageSubject == "2")
+			{
+				message.MessageSubject = "Scrum Toplantısı";
+			}
+			if (message.MessageSubject == "3")
+			{
+				message.MessageSubject = "Haftalık Değerlendirme";
+			}
+			context.Messages.Add(message);
+			context.SaveChanges();
+		}
+
+		public override void SendMessage(Message message)
+		{
+			base.SendMessage(message);
+			SendMessageByIdSubject(message);
+		}
+	}
+}
